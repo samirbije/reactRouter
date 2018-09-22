bce2c6a05b620281f54365e32703139e05e337d3
samir <samir@b;uewall.jpn.com>
2018-09-22 12:26:41 +0900

    commit for sql file

---
+++ db_techtest.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.02 
+MySQL - 5.5.5-10.1.19-MariaDB : Database - db_techtest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_techtest` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `db_techtest`;
+
+/*Table structure for table `tbl_article` */
+
+DROP TABLE IF EXISTS `tbl_article`;
+
+CREATE TABLE `tbl_article` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `created` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_article` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
